c5ed3dae68e0408dc8906eebf397244960910845
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-01-30 18:42:29 -0500

    completed controller for task

--- cadence-backend/src/task/task.controller.ts
+++ cadence-backend/src/task/task.controller.ts
@@ -1,51 +1,61 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { Category, Frequency, Task, TaskCompletion } from '@prisma/client';
+import { Category, Frequency, Prisma, Task, TaskCompletion } from '@prisma/client';
 import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly TaskService: TaskService) { }
 
-  // @Post()
-  // async task(
-  //   @Body() data: {
-  //     name: string,
-  //     createdAt: Date,
-  //     updatedAt: Date,
-  //     archived: Boolean,
-  //     archivedAt: Date,
-  //     description: string,
-  //     frequency: Frequency,
-  //     categoryId: string,
-  //     taskGroupId: string,
-  //     userId: string,
-  //     completions: TaskCompletion[]
-  //   },
-  // ): Promise<Task> {
-  //   return this.TaskService.createTask(data);
-  // }
+  @Post()
+  async task(
+    @Body() data: {
+      name: string,
+      description: string,
+      frequency: Frequency,
+      categoryId: string,
+      taskGroupId: string,
+      userId: string,
+      completions: TaskCompletion[]
+    },
+  ): Promise<Task> {
+    return this.TaskService.createTask({
+      name: data.name,
+      description: data.description,
+      frequency: data.frequency,
+      category: {
+        connect: { id: data.categoryId }
+      },
+      user: {
+        connect: { id: data.userId }
+      },
+      taskGroup: {
+        connect: { id: data.taskGroupId }
+      }
+    }
+    );
+  }
 
-  // @Get(':id')
-  // async getTaskById(@Param('id') id: string): Promise<Task | null> {
-  //   return this.TaskService.task({ id: id });
-  // }
+  @Get(':id')
+  async getTaskById(@Param('id') id: string): Promise<Task | null> {
+    return this.TaskService.task({ id: id });
+  }
 
 
-  // @Get()
-  // async getAllTaskGroups(): Promise<Task[] | null> {
-  //   return this.TaskService.tasks({})
-  // }
+  @Get()
+  async getAllTaskGroups(): Promise<Task[] | null> {
+    return this.TaskService.tasks({})
+  }
 
-  // @Patch(':id')
-  // async updateTaskGroup(@Param('id') id: string, @Body() data: { name: string, createdAt: Date, updatedAt: Date, archived: Boolean, archivedAt: Date, description: string, frequency: Frequency, categoryId: string, taskGroupId: string, userId: string, completions: TaskCompletion[] }): Promise<Task | null> {
-  //   return this.TaskService.updateTask({
-  //     where: { id },
-  //     data: data
-  //   })
-  // }
+  @Patch(':id')
+  async updateTaskGroup(@Param('id') id: string, @Body() data: Prisma.TaskUncheckedUpdateInput): Promise<Task | null> {
+    return this.TaskService.updateTask({
+      where: { id },
+      data: data
+    })
+  }
 
-  // @Delete(':id')
-  // async deleteTaskGroup(@Param('id') id: string): Promise<Task> {
-  //   return this.TaskService.deleteTask({ id })
-  // }
+  @Delete(':id')
+  async deleteTaskGroup(@Param('id') id: string): Promise<Task> {
+    return this.TaskService.deleteTask({ id })
+  }
 }
