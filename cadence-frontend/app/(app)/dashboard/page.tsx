093a4f1061f0a2b3a0e759dfb28b517120b77a23
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-03-06 22:14:16 -0500

    established layout for dashboard page

--- cadence-frontend/app/(app)/dashboard/page.tsx
+++ cadence-frontend/app/(app)/dashboard/page.tsx
@@ -7,12 +7,12 @@ export default function DashboardPage() {
   const { user } = useAuth()
 
   return <>
-    <div className="flex flex-col">
-      <h1 className="font-bold">Dashboard</h1>
-      <span className="text-muted-foreground">Welcome back, {user?.firstName}!</span>
-
+    <div className="flex flex-col gap-6 px-10 py-5 w-full">
+      <div>
+        <h1 className="font-bold">Dashboard</h1>
+        <span className="text-muted-foreground">Welcome back, {user?.firstName}!</span>
+      </div>
       <StatsCards />
-
 
     </div>
 
--- cadence-frontend/app/components/dashboard/stats-cards.tsx
+++ cadence-frontend/app/components/dashboard/stats-cards.tsx
@@ -1,9 +1,22 @@
-const stats = [
-
-]
+import { Card } from "@/components/ui/card";
 
 export function StatsCards() {
   return (
-    <div>stats cards</div>
+    <>
+      <div className="flex w-full gap-4">
+        <Card className="flex-1 h-40" />
+        <Card className="flex-1 h-40" />
+        <Card className="flex-1 h-40" />
+        <Card className="flex-1 h-40" />
+      </div>
+      <div className="flex w-full gap-4">
+        <Card className="w-[65%] h-150" />
+        <Card className="w-[35%] h-150" />
+      </div>
+      <div className="flex w-full">
+        <Card className="w-full h-90" />
+      </div>
+    </>
+
   )
 }
